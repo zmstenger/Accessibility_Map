e188e9d00751e64b5751decc2a555f2e3aed936e
Jonathan Kim <kim6jk@mail.uc.edu>
2024-10-11 19:54:28 -0400

    Changed connection string, and location model

--- backend/Models/Location.cs
+++ backend/Models/Location.cs
@@ -1,13 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace backend.Models
 {
     public class Location
     {
-        public int Id { get; set; }  // Primary Key
-        public string? Name { get; set; }  // Nullable
-        public string? Address { get; set; }  // Nullable
+        [Key]
+        public int LocationID { get; set; }
+        public string? LocationName { get; set; }  // Nullable
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public string? Accessibility { get; set; }  // Nullable
-        public int Rating { get; set; }
     }
 }
--- backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6681bab90ef1fc123fe5c96e3a85354bd7ef1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
